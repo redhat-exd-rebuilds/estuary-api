73575fc3af4b7d87bcaac3a64c55d6f344e1a234
mprahl <matthew.prahl@outlook.com>
2018-03-19 16:27:07 -0400

    Add the Errata and Freshmaker models

---
+++ purview/models/errata.py
@@ -0,0 +1,27 @@
+from neomodel import (
+    StructuredNode, UniqueIdProperty, RelationshipTo, IntegerProperty,
+    StringProperty, BooleanProperty, ArrayProperty)
+
+
+class Advisory(StructuredNode):
+    id_ = UniqueIdProperty(db_property='id')
+    triggers_freshmaker_event = RelationshipTo(
+        '.freshmaker.FreshmakerEvent', 'TRIGGERS')
+    triggers_container_builds = RelationshipTo(
+        '.freshmaker.ContainerBuilds', 'TRIGGERS')
+    advisory_name = StringProperty(unique=True, required=True)
+    status = StringProperty(required=True)
+    # product = Property() # a dict that should be broken out
+    text_only = BooleanProperty()
+    # people = Property() # a dict that should be broken out
+    content_types = ArrayProperty()
+    pushcount = IntegerProperty()
+    synopsis = StringProperty(required=True)
+    respin_count = IntegerProperty()
+    security_impact = StringProperty(required=True)
+    # flags = Property() # a dict that should be broken out
+    # release = Property() # a dict that should be broken out
+    # timestamps = Property() # a dict that should be broken out
+    # content = Property() # a dict that should be broken out
+    type_ = StringProperty(required=True, db_property='type')
+    revision = IntegerProperty(require=True)
---
+++ purview/models/freshmaker.py
@@ -0,0 +1,39 @@
+from neomodel import (
+    StructuredNode, UniqueIdProperty, RelationshipFrom, RelationshipTo,
+    IntegerProperty, StringProperty, DateTimeProperty)
+
+
+class FreshmakerEvent(StructuredNode):
+    id_ = UniqueIdProperty(db_property='id')
+    triggered_by_advisory = RelationshipFrom(
+        '.errata.Advisory', 'TRIGGERED_BY')
+    triggers_container_builds = RelationshipTo('ContainerBuilds', 'TRIGGERS')
+    event_type_id = IntegerProperty(requried=True)
+    message_id = StringProperty(unique=True, required=True)
+    search_key = StringProperty(unique=True, required=True)
+    state = IntegerProperty(required=True)
+    state_name = StringProperty(required=True)
+    state_reason = StringProperty()
+    url = StringProperty(unique=True, required=True)
+
+
+class ContainerBuilds(StructuredNode):
+    id_ = UniqueIdProperty(db_property='id')
+    triggered_by_advisory = RelationshipFrom(
+        '.errata.Advisory', 'TRIGGERED_BY')
+    triggered_by_freshmaker_event = RelationshipFrom(
+        'FreshmakerEvent', 'TRIGGERED_BY')
+    build_id = IntegerProperty(unique=True, required=True)
+    dep_on = StringProperty()
+    event_id = IntegerProperty(required=True)
+    name = StringProperty(required=True)
+    original_nvr = StringProperty(required=True)
+    rebuilt_nvr = StringProperty(required=True)
+    state = IntegerProperty(required=True)
+    state_name = StringProperty(required=True)
+    state_reason = StringProperty(required=True)
+    time_completed = DateTimeProperty()
+    time_submitted = DateTimeProperty(required=True)
+    type_ = IntegerProperty(required=True, db_property='type')
+    type_name = StringProperty(required=True)
+    url = StringProperty(required=True)
