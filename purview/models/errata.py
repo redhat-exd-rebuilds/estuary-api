8c9f593263b5f6e46a05cabe97ccdd4e878cbbbc
mprahl <matthew.prahl@outlook.com>
2018-03-21 17:51:23 -0400

    Add license headers

--- purview/models/errata.py
+++ purview/models/errata.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: GPL-3.0+
+
 from neomodel import (
     StructuredNode, UniqueIdProperty, RelationshipTo, IntegerProperty,
     StringProperty, BooleanProperty, ArrayProperty)
--- purview/utils/teiid.py
+++ purview/utils/teiid.py
@@ -1,4 +1,6 @@
+# SPDX-License-Identifier: GPL-3.0+
 # Inspired from the MARS project
+
 import psycopg2
 import logging
 
