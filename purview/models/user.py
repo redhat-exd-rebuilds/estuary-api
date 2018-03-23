1fb99eaa281dd60fdf567bca239aead2d1033c60
Yashvardhan <yashn@bu.edu>
2018-03-23 16:09:47 -0400

    User model updated

--- purview/models/user.py
+++ purview/models/user.py
@@ -7,7 +7,7 @@ from neomodel import (
 
 class User(StructuredNode):
     username = UniqueIdProperty()
-    owns_builds = RelationshipTo('.koji.KojiBuild', 'OWNS')
-    owns_tasks = RelationshipTo('.koji.KojiTask', 'OWNS')
+    koji_builds = RelationshipTo('.koji.KojiBuild', 'OWNS')
+    koji_tasks = RelationshipTo('.koji.KojiTask', 'OWNS')
     name = StringProperty()
     email = StringProperty()
