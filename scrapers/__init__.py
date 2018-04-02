4021f23777d0a05e04c888d52423541eefd90ec1
mprahl <mprahl@users.noreply.github.com>
2018-04-02 09:14:08 -0400

    Add the ability to run all the scrapers in one command

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -1,7 +1,10 @@
 # SPDX-License-Identifier: GPL-3.0+
 # Import all scrapers here so that the scrape script can find them
-from scrapers.koji import KojiScraper  # noqa: F401
-from scrapers.distgit import DistGitScraper  # noqa: F401
-from scrapers.bugzilla import BugzillaScraper  # noqa: F401
-from scrapers.errata import ErrataScraper  # noqa: F401
-from scrapers.freshmaker import FreshmakerScraper  # noqa: F401
+from scrapers.koji import KojiScraper
+from scrapers.distgit import DistGitScraper
+from scrapers.bugzilla import BugzillaScraper
+from scrapers.errata import ErrataScraper
+from scrapers.freshmaker import FreshmakerScraper
+
+# Define the scrapers here in the order that they should be run
+all_scrapers = (BugzillaScraper, DistGitScraper, KojiScraper, ErrataScraper, FreshmakerScraper)
--- scripts/scrape.py
+++ scripts/scrape.py
@@ -9,14 +9,14 @@ import os
 # So we can import the scrapers module
 sys.path.insert(1, os.path.abspath(os.path.join(sys.path[0], '..')))
 
-import scrapers  # noqa: E402
+from scrapers import all_scrapers  # noqa: E402
 
 logging.basicConfig(format='[%(filename)s:%(lineno)s:%(funcName)s] %(message)s')
 log = logging.getLogger('purview')
 log.setLevel(logging.DEBUG)
 
 parser = argparse.ArgumentParser(description='Run a scraper')
-parser.add_argument('scraper', type=str, help='The scraper to run')
+parser.add_argument('scraper', type=str, help='The scraper to run. To run them all, use "all".')
 parser.add_argument('--since', type=str, help='A UTC date formatted in "yyyy-mm-dd" limit results')
 parser.add_argument('--teiid-user', type=str, help='The Teiid user')
 parser.add_argument('--teiid-password', type=str, help='The Teiid password')
@@ -27,20 +27,26 @@ parser.add_argument('--neo4j-server', type=str, default='localhost',
 parser.add_argument('--kerberos', action='store_true', help='Use Kerberos for authentication')
 args = parser.parse_args()
 
-log.debug('Searching for the "{0}" scraper'.format(args.scraper))
-scraper = None
-for item in dir(scrapers):
-    if item.endswith('Scraper') and item[0:-7].lower() == args.scraper.lower():
-        log.debug('Matched "scraper.{0}" to the desired scraper of "{1}"'.format(
-            item, args.scraper))
-        scraper = getattr(scrapers, item)(
-            args.teiid_user, args.teiid_password, args.kerberos, args.neo4j_user,
-            args.neo4j_password, args.neo4j_server)
-        break
+scraper_classes = tuple()
+if args.scraper.lower() == 'all':
+    log.debug('Running all the scrapers in order')
+    scraper_classes = all_scrapers
+else:
+    log.debug('Searching for the "{0}" scraper'.format(args.scraper))
+    for item in all_scrapers:
+        if item.__name__[0:-7].lower() == args.scraper.lower():
+            log.debug('Matched "scraper.{0}" to the desired scraper of "{1}"'.format(
+                item.__name__, args.scraper))
+            scraper_classes = tuple([item])
+            break
 
-if scraper is None:
+if not scraper_classes:
     error = 'A scraper for "{0}" couldn\'t be found'.format(args.scraper)
     log.error(error)
     raise RuntimeError(error)
 
-scraper.run(since=args.since)
+for scraper_class in scraper_classes:
+    scraper = scraper_class(
+        args.teiid_user, args.teiid_password, args.kerberos, args.neo4j_user, args.neo4j_password,
+        args.neo4j_server)
+    scraper.run(since=args.since)
