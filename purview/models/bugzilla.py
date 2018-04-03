16620271355bc554ca0c5ce84be129d65d5a1477
Yashvardhan <yashn@bu.edu>
2018-04-03 17:01:42 -0400

    Re-organize model classes

--- purview/models/bugzilla.py
+++ purview/models/bugzilla.py
@@ -8,26 +8,26 @@ from neomodel import (
 
 
 class BugzillaBug(StructuredNode):
-    id_ = UniqueIdProperty(db_property='id')
-    owners = RelationshipTo('.user.User', 'OWNED_BY')  # same as the reporter
     assignees = RelationshipTo('.user.User', 'ASSIGNED_TO')
-    reporters = RelationshipTo('.user.User', 'REPORTED_BY')
-    qa_contacts = RelationshipTo('.user.User', 'QA_BY')
-    related_by_commits = RelationshipTo('.distgit.DistGitCommit', 'RELATED_TO')
-    resolved_by_commits = RelationshipTo('.distgit.DistGitCommit', 'RESOLVED_BY')
-    reverted_by_commits = RelationshipTo('.distgit.DistGitCommit', 'REVERTED_BY')
     attached_advisories = RelationshipTo('.errata.Advisory', 'RELATED_TO')
-    severity = StringProperty()
-    status = StringProperty()
+    # This will always be "Red Hat" initially
+    classification = StringProperty()
     creation_time = DateTimeProperty()
+    id_ = UniqueIdProperty(db_property='id')
     modified_time = DateTimeProperty()  # delta_ts
+    owners = RelationshipTo('.user.User', 'OWNED_BY')  # same as the reporter
     priority = StringProperty()
     # Called product_name in case we want to use product as a relationship later on
     product_name = StringProperty()
     product_version = StringProperty()
-    # This will always be "Red Hat" initially
-    classification = StringProperty()
+    qa_contacts = RelationshipTo('.user.User', 'QA_BY')
+    related_by_commits = RelationshipTo('.distgit.DistGitCommit', 'RELATED_TO')
+    reporters = RelationshipTo('.user.User', 'REPORTED_BY')
     resolution = StringProperty()
+    resolved_by_commits = RelationshipTo('.distgit.DistGitCommit', 'RESOLVED_BY')
+    reverted_by_commits = RelationshipTo('.distgit.DistGitCommit', 'REVERTED_BY')
+    severity = StringProperty()
+    short_description = StringProperty()
+    status = StringProperty()
     target_milestone = StringProperty()
     votes = IntegerProperty()
-    short_description = StringProperty()
