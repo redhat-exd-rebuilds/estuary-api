ec2218a0bb58c551245fda1d1170dd3359e73564
mprahl <mprahl@users.noreply.github.com>
2018-04-16 12:17:55 -0400

    Breakout code from api.v1.get_resource into a separate function

--- purview/api/v1.py
+++ purview/api/v1.py
@@ -4,12 +4,9 @@ from __future__ import unicode_literals
 
 from flask import Blueprint, jsonify, request
 from werkzeug.exceptions import NotFound
-from neomodel import UniqueIdProperty
 
 from purview import version
-from purview.models import all_models
-from purview.error import ValidationError
-from purview.utils.general import str_to_bool
+from purview.utils.general import str_to_bool, get_neo4j_node
 
 api_v1 = Blueprint('api_v1', __name__)
 
@@ -41,23 +38,11 @@ def get_resource(resource, uid):
     if request.args.get('relationship'):
         relationship = str_to_bool(request.args['relationship'])
 
-    for model in all_models:
-        if model.__name__.lower() == resource:
-            for prop_name, prop_def in model.defined_properties().items():
-                if isinstance(prop_def, UniqueIdProperty):
-                    item = model.nodes.get_or_none(**{prop_def.name: uid})
-                    if not item:
-                        raise NotFound('This item does not exist')
+    item = get_neo4j_node(resource, uid)
+    if not item:
+        raise NotFound('This item does not exist')
 
-                    if relationship:
-                        return jsonify(item.serialized_all)
-                    else:
-                        return jsonify(item.serialized)
-
-    # Some models don't have unique ID's and those should be skipped
-    models_wo_uid = ('DistGitRepo', 'DistGitBranch')
-    model_names = [model.__name__.lower() for model in all_models
-                   if model.__name__ not in models_wo_uid]
-    error = ('The requested resource "{0}" is invalid. Choose from the following: '
-             '{1}, and {2}.'.format(resource, ', '.join(model_names[:-1]), model_names[-1]))
-    raise ValidationError(error)
+    if relationship:
+        return jsonify(item.serialized_all)
+    else:
+        return jsonify(item.serialized)
