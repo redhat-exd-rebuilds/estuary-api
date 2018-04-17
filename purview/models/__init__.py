6e7a0b43c1869a17710a5f1f5d0ed176b689d067
mprahl <mprahl@users.noreply.github.com>
2018-04-17 16:32:00 -0400

    Monkey patch connect methods with the ones in neomodel#326

--- purview/models/__init__.py
+++ purview/models/__init__.py
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: GPL-3.0+
 
+from neomodel import ZeroOrOne, One, AttemptedCardinalityViolation
+
 from purview.models.koji import KojiBuild, KojiTask, KojiTag
 from purview.models.bugzilla import BugzillaBug
 from purview.models.distgit import DistGitRepo, DistGitPush, DistGitBranch, DistGitCommit
@@ -55,3 +57,27 @@ story_flow = {
     }
 
 }
+
+
+# Overrides from https://github.com/neo4j-contrib/neomodel/pull/326
+# These should be removed once the PR is merged and a new version is released
+def zero_or_one_connect(self, node, properties=None):
+    """Override the connect method with code in PR #326."""
+    if len(self) and node not in self:
+        raise AttemptedCardinalityViolation('Node already has {0} can\'t connect more'.format(self))
+    else:
+        return super(ZeroOrOne, self).connect(node, properties)
+
+
+def one_connect(self, node, properties=None):
+    """Override the connect method with code in PR #326."""
+    if not hasattr(self.source, 'id'):
+        raise ValueError('Node has not been saved cannot connect!')
+    if len(self) and node not in self:
+        raise AttemptedCardinalityViolation('Node already has one relationship')
+    else:
+        return super(One, self).connect(node, properties)
+
+
+ZeroOrOne.connect = zero_or_one_connect
+One.connect = one_connect
