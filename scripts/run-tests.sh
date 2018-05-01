932e2eade600c310ba05c04734bcb5bbcbe437cd
mprahl <mprahl@users.noreply.github.com>
2018-05-01 10:50:50 -0400

    Install the APOC plugin for CI

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -5,6 +5,11 @@ for dir in purview tests; do
     find ${dir} -type f \( -name '*.pyc' -or -name '*.pyc' \) -exec rm -f {} \;
 done
 
+if [ ! -f /tmp/neo4j-plugins/apoc-3.3.0.2-all.jar ]; then
+    mkdir -p /tmp/neo4j-plugins
+    curl -L https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/3.3.0.2/apoc-3.3.0.2-all.jar -o /tmp/neo4j-plugins/apoc-3.3.0.2-all.jar
+fi
+
 docker-compose -f docker/docker-compose-tests.yml up -d neo4j
 if [ "$1" != "" ]; then
     # The user passed at least one argument, so use those arguments as the run command instead
