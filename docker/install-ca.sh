e0f7816e7c0695c2b4cb63e3f08df03d9f84240b
mprahl <mprahl@users.noreply.github.com>
2018-05-17 13:47:21 -0400

    Break out the CA installation commands to a separate script for other images to reuse it

--- docker/install-ca.sh
+++ docker/install-ca.sh
@@ -6,11 +6,3 @@ if [ -n "${CA_URL}" ] && [ ! -f "/tmp/.imported" ]; then
     # Create a file so we know not to import it again if the container is restarted
     touch /tmp/.imported
 fi
-
-exec python3 scripts/scrape.py $SCRAPER \
-    --teiid-user $TEIID_USER \
-    --teiid-password $TEIID_PASSWORD \
-    --neo4j-user $NEO4J_USER \
-    --neo4j-password $NEO4J_PASSWORD \
-    --neo4j-server $NEO4J_SERVER \
-    --days-ago $SCRAPE_FROM_DAYS_AGO
