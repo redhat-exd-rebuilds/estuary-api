52da1c71db7cea13f62e7d5f314c4b597d168a1d
mprahl <mprahl@users.noreply.github.com>
2018-06-07 15:14:29 -0400

    Support date ranges in the scraper Docker image

---
+++ docker/scrape.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+args=''
+if [ -n "${SCRAPE_SINCE}" ]; then
+    args+="--since ${SCRAPE_SINCE} "
+fi
+if [ -n "${SCRAPE_UNTIL}" ]; then
+    args+="--until ${SCRAPE_UNTIL}"
+fi
+if [ ! -n "${args}" ]; then
+    args="--days-ago ${SCRAPE_FROM_DAYS_AGO}"
+fi
+
+docker/install-ca.sh && \
+    sleep $WAIT_FOR && \
+    exec python3 scripts/scrape.py $SCRAPER \
+        --teiid-user $TEIID_USER \
+        --teiid-password $TEIID_PASSWORD \
+        --neo4j-user $NEO4J_USER \
+        --neo4j-password $NEO4J_PASSWORD \
+        --neo4j-server $NEO4J_SERVER \
+        $args
