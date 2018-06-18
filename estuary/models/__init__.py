0603fed2cc395a92aad63bd8805e70547a390a79
Yashvardhan <yashn@bu.edu>
2018-06-18 13:35:58 -0400

    API fix to add ContainerAdvisory artifact to the story/path

--- estuary/models/__init__.py
+++ estuary/models/__init__.py
@@ -3,13 +3,14 @@
 from estuary.models.koji import ContainerKojiBuild, KojiBuild, KojiTask, KojiTag
 from estuary.models.bugzilla import BugzillaBug
 from estuary.models.distgit import DistGitRepo, DistGitPush, DistGitBranch, DistGitCommit
-from estuary.models.errata import Advisory, AdvisoryState
+from estuary.models.errata import Advisory, AdvisoryState, ContainerAdvisory
 from estuary.models.freshmaker import FreshmakerEvent
 from estuary.models.user import User
 
-all_models = (Advisory, AdvisoryState, BugzillaBug, ContainerKojiBuild, DistGitBranch,
-              DistGitCommit, DistGitPush, DistGitRepo, FreshmakerEvent, KojiBuild,
-              KojiTag, KojiTask, User)
+all_models = (Advisory, AdvisoryState, BugzillaBug, ContainerAdvisory, ContainerKojiBuild,
+              DistGitBranch, DistGitCommit, DistGitPush, DistGitRepo, FreshmakerEvent,
+              KojiBuild, KojiTag, KojiTask, User)
 names_to_model = {model.__label__: model for model in all_models}
 story_flow_list = ['BugzillaBug', 'DistGitCommit', 'KojiBuild',
-                   'Advisory', 'FreshmakerEvent', 'ContainerKojiBuild']
+                   'Advisory', 'FreshmakerEvent', 'ContainerKojiBuild',
+                   'ContainerAdvisory']
