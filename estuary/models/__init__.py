4394629b30dd27b72bed7710b984899e22217b5a
Yashvardhan <yashn@bu.edu>
2018-06-21 16:38:06 -0400

    Add dict to store inheritance between model classes
    Co-authored-by: mprahl <mprahl@users.noreply.github.com>

--- estuary/models/__init__.py
+++ estuary/models/__init__.py
@@ -14,3 +14,22 @@ names_to_model = {model.__label__: model for model in all_models}
 story_flow_list = ['BugzillaBug', 'DistGitCommit', 'KojiBuild',
                    'Advisory', 'FreshmakerEvent', 'ContainerKojiBuild',
                    'ContainerAdvisory']
+
+
+def _get_models_inheritance():
+    """
+    Create a dictionary mapping the model inheritance.
+
+    :return: a dictionary with the keys as model labels and the values as labels inherited from it
+    :rtype: dict
+    """
+    models_inheritance = {}
+    for curr_model in all_models:
+        models_inheritance[curr_model.__label__] = set([curr_model.__label__])
+        for next_model in all_models:
+            if next_model.__label__.endswith(curr_model.__label__):
+                models_inheritance[curr_model.__label__].add(next_model.__label__)
+    return models_inheritance
+
+
+models_inheritance = _get_models_inheritance()
