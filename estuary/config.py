8fd228c4f858640ab9f89d4ed4b4f654895308cd
mprahl <mprahl@users.noreply.github.com>
2018-09-11 15:25:51 -0400

    Add optional OpenID Connect authentication

--- estuary/config.py
+++ estuary/config.py
@@ -10,12 +10,20 @@ class Config(object):
     # We configure logging explicitly, turn off the Flask-supplied log handler.
     LOGGER_HANDLER_POLICY = 'never'
     HOST = '0.0.0.0'
+    # This is normally the path for the flask-oidc config, but it isn't used. It must be defined
+    # however.
+    OIDC_CLIENT_SECRETS = None
+    OIDC_RESOURCE_SERVER_ONLY = True
     PRODUCTION = False
     SHOW_DB_URI = False
     SECRET_KEY = 'replace-me-with-something-random'
     NEO4J_URI = 'bolt://neo4j:neo4j@localhost:7687'
     CORS_URL = '*'
     STORY_MANAGER_SEQUENCE = ['ModuleStoryManager', 'ContainerStoryManager']
+    ENABLE_AUTH = False
+    OIDC_INTROSPECT_URL = None
+    OIDC_CLIENT_ID = None
+    OIDC_CLIENT_SECRET = None
 
 
 class ProdConfig(Config):
@@ -34,4 +42,13 @@ class DevConfig(Config):
 class TestConfig(Config):
     """The test Estuary application configuration."""
 
-    pass
+    ENABLE_AUTH = False
+
+
+class TestAuthConfig(TestConfig):
+    """The test Estuary application configuration with authentication configured."""
+
+    OIDC_INTROSPECT_URL = 'https://provider.domain.local/oauth2/default/v1/introspect'
+    OIDC_CLIENT_ID = 'estuary'
+    OIDC_CLIENT_SECRET = 'some_secret'
+    ENABLE_AUTH = True
