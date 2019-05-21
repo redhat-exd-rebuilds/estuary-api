2e460fa715d1fac89fe59e6454f1b93666b662db
sarah256 <srieger@bu.edu>
2019-05-21 17:25:22 -0400

    Update container base images from Fedora 28 to Fedora 30

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,9 +8,6 @@ from neomodel import config as neomodel_config, db as neo4j_db
 
 from estuary.app import create_app
 
-neomodel_config.DATABASE_URL = os.environ.get('NEO4J_BOLT_URL', 'bolt://neo4j:neo4j@localhost:7687')
-neomodel_config.AUTO_INSTALL_LABELS = True
-
 
 @pytest.fixture(scope='session')
 def client():
@@ -23,4 +20,8 @@ def client():
 def run_before_tests():
     """Pytest fixture that prepares the environment before each test."""
     # Code that runs before each test
+    neomodel_config.DATABASE_URL = os.environ.get(
+        'NEO4J_BOLT_URL', 'bolt://neo4j:neo4j@localhost:7687'
+    )
+    neomodel_config.AUTO_INSTALL_LABELS = True
     neo4j_db.cypher_query('MATCH (a) DETACH DELETE a')
