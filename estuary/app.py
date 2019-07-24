0062a6f21faf179101af5a5e9dcb95012a7a48a1
sarah256 <srieger@bu.edu>
2019-07-24 10:45:49 -0400

    Fix a typo in the Access-Control-Allow-Methods header

--- estuary/app.py
+++ estuary/app.py
@@ -71,7 +71,7 @@ def insert_headers(response):
     if origin and origin in cors_origins:
         response.headers['Access-Control-Allow-Origin'] = origin
         response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-        response.headers['Access-Control-Allow-Method'] = 'GET, OPTIONS'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
     return response
 
 
