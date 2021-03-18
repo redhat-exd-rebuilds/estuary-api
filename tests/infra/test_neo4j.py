5860128e31bc8ac2dddcedeac7641f77b550ec7f
Matt Prahl <mprahl@users.noreply.github.com>
2021-03-18 11:04:21 -0400

    Bump neo4j release to the latest 3 release
    For competability with the current release running in prod environment

---
+++ tests/infra/test_neo4j.py
@@ -0,0 +1,11 @@
+import docker
+
+
+def test_apoc_plugin_installed():
+    client = docker.from_env()
+    db_container = client.containers.get('db')
+    res = db_container.exec_run(
+        cmd="test -f /var/lib/neo4j/plugins/apoc.jar",
+        tty=True
+    )
+    assert res[0] == 0
