4b8c597311daf2cc16592cae9682e0e228472aa3
mprahl <mprahl@users.noreply.github.com>
2021-04-13 12:02:50 -0400

    Fix flake8 errors

--- tests/unit/test_ProductVersion.py
+++ tests/unit/test_ProductVersion.py
@@ -5,6 +5,6 @@ from estuary.models.productversion import ProductVersion
 
 @pytest.mark.parametrize("name", [("8.4.0.0"), ("8.5.0.0")])
 def test_ProductVersion(name):
-    """Test ProductVersion"""
+    """Test ProductVersion."""
     productversion = ProductVersion(name=name).save()
     assert productversion.name == name
